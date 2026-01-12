d63b89f5c3151bbfeca14c274e31e0ff74e4d383
JensenRasmus <rasmusj@imada.sdu.dk>
2026-01-12 15:01:42 +0100

    Update

---
+++ FN_interpolate_update.m
@@ -0,0 +1,134 @@
+%% Interpolate the FN system
+%
+% Interpolate the POD basis of the u-component of the FN system
+%   Data: Contains U 
+%   dData: Contains the dU's, which are all horizontal.
+%   Data_ref: Contains reference U data, matching the U for each m_h entry
+%               eg. Data_ref{m_k*k + 1).U = Data{k}.U
+function FN_interpolate_update(Data, dData, Data_ref, points, m_h, p, maxsteps)
+
+M = matrix_tools();
+
+E_lag_loc = [];
+E_herm_loc = [];
+E_lag_norm = [];
+E_herm_norm = [];
+
+% Interpolate piece-wise on points(j) -- points(j+1)
+
+l = length(points);
+
+
+% Normal coordinates
+for i = 1:(l-1)
+    ts = linspace(points(i),points(i+1),m_h+1);
+    Ds = Data(i:(i+1));
+    dDs = dData(i:(i+1));
+    Dref = Data_ref((i-1)*m_h + 1:m_h*i+1);
+    for j = 1:m_h+1
+        Ptrue = Dref{j}(:,1:p)*Dref{j}(:,1:p)';
+        Uint = Interpolate_Gr([points(i) points(i+1)],Ds,ts(j),'normal_lag');
+        e = norm(Uint*Uint' - Ptrue,'fro') / norm(Ptrue,'fro');
+        E_lag_norm = [E_lag_norm e];
+    end
+    if i ~= l-1
+        E_lag_norm = E_lag_norm(1:end-1);
+    end
+end
+
+for i = 1:(l-1)
+    ts = linspace(points(i),points(i+1),m_h+1);
+    Ds = Data(i:(i+1));
+    dDs = dData(i:(i+1));
+    Dref = Data_ref((i-1)*m_h + 1:m_h*i+1);
+    for j = 1:m_h+1
+        Ptrue = Dref{j}(:,1:p)*Dref{j}(:,1:p)';
+        Uint = Interpolate_Gr([points(i) points(i+1)],Ds,ts(j),'normal_herm',dDs);
+        e = norm(Uint*Uint' - Ptrue,'fro') / norm(Ptrue,'fro');
+        E_herm_norm = [E_herm_norm e];
+    end
+    if i ~= l-1
+        E_herm_norm = E_herm_norm(1:end-1);
+    end
+end
+
+
+% Maxvol coordinates
+% First we compute the maximum-volume 
+% One has to selet the permutation matrix resulting in the smallest maximal
+% conditon number, for each pair of consecutive data points. 
+% For this example, it is seen that it makes no large difference if one
+% chooses the P obtained at entry s = 1 or s = 2. Choose s = 1.
+conds = zeros(l,4);
+P = cell(1,l-1);
+for i = 1:(l-1)
+    ss = i:(i+1);
+    Ps = cell(1,2);
+    for s = 1:2
+        [~,Ps{s}] = maxvol(Data{ss(s)},maxsteps);
+        %conds(i,s) = cond(Data{ss(s)}(1:p,1:p),'fro');
+        PU = Ps{s}*Data{ss(1)};
+        conds(i,(s-1)*2+1) = cond(PU(1:p,1:p) ,'fro');
+        PU = Ps{s}*Data{ss(2)};
+        conds(i,(s-1)*2+2) = cond(PU(1:p,1:p),'fro');
+    end
+    P{i} = Ps{1};
+end
+
+
+for i = 1:(l-1)
+    ts = linspace(points(i),points(i+1),m_h+1);
+    Ds = Data(i:(i+1));
+    dDs = dData(i:(i+1));
+
+    % Apply P
+    for s = 1:2
+        Ds{s} = P{i}*Ds{s};
+        dDs{s} = P{i}*dDs{s};
+    end
+    Dref = Data_ref((i-1)*m_h + 1:m_h*i+1);
+    for j = 1:m_h+1
+        Ptrue = Dref{j}(:,1:p)*Dref{j}(:,1:p)';
+        Uint = Interpolate_Gr([points(i) points(i+1)],Ds,ts(j),'local_lag');
+        Uint = P{i}'*Uint;
+        e = norm(Uint*Uint' - Ptrue,'fro') / norm(Ptrue,'fro');
+        E_lag_loc = [E_lag_loc e];
+    end
+    if i ~= l-1
+        E_lag_loc = E_lag_loc(1:end-1);
+    end
+end
+
+for i = 1:(l-1)
+    ts = linspace(points(i),points(i+1),m_h+1);
+    Ds = Data(i:(i+1));
+    dDs = dData(i:(i+1));
+    norm(Ds{s}'*dDs{s});
+    % Apply P
+    for s = 1:2
+        Ds{s} = P{i}*Ds{s};
+        dDs{s} = P{i}*dDs{s};
+    end
+    Dref = Data_ref((i-1)*m_h + 1:m_h*i+1);
+    for j = 1:m_h+1
+        Ptrue = Dref{j}(:,1:p)*Dref{j}(:,1:p)';
+        Uint = Interpolate_Gr([points(i) points(i+1)],Ds,ts(j),'local_herm',dDs);
+        Uint = P{i}'*Uint;
+        e = norm(Uint*Uint' - Ptrue,'fro') / norm(Ptrue,'fro');
+        E_herm_loc = [E_herm_loc e];
+    end
+    if i ~= l-1
+        E_herm_loc = E_herm_loc(1:end-1);
+    end
+end
+
+
+figure
+% plot(E_lag_loc)
+% hold on
+% plot(E_lag_norm)
+hold on
+plot(E_herm_loc)
+plot(E_herm_norm)
+
+end
